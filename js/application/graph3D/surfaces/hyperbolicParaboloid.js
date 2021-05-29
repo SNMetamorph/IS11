3da2698a923ab7ab3989771c04b0f7b90c10fe38
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2021-05-29 10:44:28 +0400

    Implemented hyperbolic paraboloid subject

--- js/application/graph3D/surfaces/hyperbolicParaboloid.js
+++ js/application/graph3D/surfaces/hyperbolicParaboloid.js
@@ -1,20 +1,44 @@
 // гиперболический параболоид
-Surface.prototype.hyperbolicParaboloid = (x = 0, y = 0, z = 0, pointCount = 20) => {
+Surface.prototype.hyperbolicParaboloid = (px = 0, py = 0, pz = 0, p = 2, q = 2, count = 20) => {
     const points = [];
     const edges = [];
     const polygones = [];
 
-    function createPoints() {              
+    const LEFT = -10;
+    const RIGHT = 10;
+    const step = (RIGHT - LEFT) / count;
+
+    // add points
+    for (let x = LEFT; x < RIGHT; x += step) {
+        for (let y = LEFT; y < RIGHT; y += step) {
+            points.push(new Point(
+                px + x,
+                py + (x ** 2 / p - y ** 2 / q) / 2,
+                pz + y
+            ));
+        }
     }
 
-    function createEdges() {
+    // add edges 
+    for (let i = 0; i < points.length; ++i) {
+        if (points[i + 1] && (i + 1) % count !== 0) {
+            edges.push(new Edge(i, i + 1));
+        }
+        if (points[i + count]) {
+            edges.push(new Edge(i, i + count));
+        }
     }
 
-    function createPolygons() {
+    // add polygones
+    for (let i = 0; i < points.length; ++i) {
+        if (points[i + 1 + count] && (i + 1) % count !== 0) {
+            polygones.push(new Polygon([
+                i, 
+                i + 1,
+                i + 1 + count,
+                i + count
+            ]));
+        }
     }
-
-    createPoints();
-    createEdges();
-    createPolygons();
     return new Subject(points, edges, polygones);
 };
