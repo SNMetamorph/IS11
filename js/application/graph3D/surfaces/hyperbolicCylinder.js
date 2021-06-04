bfd1fb2970f4a1df49d3d84b0bcd6cea0c335694
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2021-06-04 22:58:14 +0400

    Implemented hyperbolic cylinder (only points/edges)

--- js/application/graph3D/surfaces/hyperbolicCylinder.js
+++ js/application/graph3D/surfaces/hyperbolicCylinder.js
@@ -1,16 +1,41 @@
 // гиперболический цилиндр
-Surface.prototype.hyperbolicCylinder = (x = 0, y = 0, z = 0, pointCount = 20) => {
+Surface.prototype.hyperbolicCylinder = (x = 0, y = 0, z = 0, w = 4, h = 8, count = 10) => {
     const points = [];
     const edges = [];
     const polygones = [];
 
-    function createPoints() {              
+    function createPoints() { 
+        const step = w / count;
+        for (let pz = 0; pz <= h; pz += h / count) {
+            for (let px = step; px <= w; px += step) {
+                points.push(new Point(x + px, y + 1 / px, z + pz));
+            }
+        }
+        for (let pz = 0; pz <= h; pz += h / count) {
+            for (let px = -step; px >= -w; px -= step) {
+                points.push(new Point(x + px, y + 1 / px, z + pz));
+            }
+        }
     }
 
     function createEdges() {
+        for (let i = 0; i < points.length; ++i) {
+            if ((i+1) % count != 0) {
+                edges.push(new Edge(i, i+1));
+            }
+        }
+        for (let k = 0; k < 2; ++k) {
+            let offset = k * (count * (count + 1));
+            for (let i = 0; i < count; ++i) {
+                for (let j = 0; j < count; ++j) {
+                    edges.push(new Edge(offset + i + j * count, offset + i + (j+1) * count));
+                }
+            }
+        }
     }
 
     function createPolygons() {
+
     }
 
     createPoints();
